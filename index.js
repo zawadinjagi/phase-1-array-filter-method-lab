470b128a743a89d0012c6796eddc294cc54e585b
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-25 11:33:58 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver =>
+      driver.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+  
